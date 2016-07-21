81b5a79ba18a9c0cb53559c161c35b10f71a0e84
tauyama <godfaceo@gmail.com>
2016-07-21 13:50:18 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -32,7 +32,7 @@ $app->post('/callback', function (Request $request) use ($app, $bot) {
         $content = $obj['content'];
 
         if ($content['text']) {
-            $bot->sendText($from, sprintf('%s%d年%d月%d日ですa', $content['text'],date( "Y" , $time ),date( "m" , $time ),date( "d" , $time ),date( "G" , $time ),date( "i" , $time ))); 
+            $bot->sendText($from, sprintf('%s%d年%d月%d日%d時%d分ですa', $content['text'],date( "Y" , $time ),date( "m" , $time ),date( "d" , $time ),date( "G" , $time ),date( "i" , $time ))); 
             $bot->sendText($from, sprintf('%s%dですb', $content['text'],date( DATE_ATOM )));
         }
     }
