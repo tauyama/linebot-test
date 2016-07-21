08b6fef50e5ed296d5c9d3cf34490327891e9f53
tauyama <godfaceo@gmail.com>
2016-07-21 15:26:17 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -27,7 +27,8 @@ $app->post('/callback', function (Request $request) use ($app, $bot) {
     $time = time() ;
     $filename = 'test.txt';
     $fp = fopen($filename, 'r');
-    $file = file_get_contents($filename)
+    $file = file_get_contents($filename);
+    $aa=readfile($filename);
     
     foreach ($body['result'] as $obj) {
         $app['monolog']->addInfo(sprintf('obj: %s', json_encode($obj)));
@@ -36,7 +37,7 @@ $app->post('/callback', function (Request $request) use ($app, $bot) {
 
         if ($content['text']) {
             $bot->sendText($from, sprintf('%s%d年%d月%d日%d時%d分ですa', $content['text'],date( "Y" , $time ),date( "m" , $time ),date( "d" , $time ),date( "G" , $time ),date( "i" , $time )));
-            $bot->sendText($from, sprintf('%sあ%s', $content['text'],readfile($filename)));
+            $bot->sendText($from, sprintf('%sあ%s', $content['text'],$file));
         }
     }
 
